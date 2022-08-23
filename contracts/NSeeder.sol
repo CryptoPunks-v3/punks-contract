bee35b986583772407e8bd36810420f026da89e0
iforcenick <felixsoft.dev@gmail.com>
2022-08-23 06:39:07 -0400

    feat: got acc generation info

--- contracts/NSeeder.sol
+++ contracts/NSeeder.sol
@@ -6,9 +6,14 @@ import "./interfaces/ISeeder.sol";
 
 contract NSeeder is ISeeder, Ownable {
     
+    // MAX_GROUP_COUNT = 16
     uint24[] public cTypeProbability;
     uint24[][] public cSkinProbability;
     uint24[] public cAccProbability;
+    uint256[] public accFlags;
+    uint256 accCount;
+    mapping(uint256 => uint256) public accGroupMapping; // i: acc index, group index
+    uint256[][] accGroup; // i: group id, j: acc index in a group
     
     constructor() {
     }
@@ -20,6 +25,7 @@ contract NSeeder is ISeeder, Ownable {
 
         Seed memory seed;
 
+        // Pick up random punk type
         uint24 partRandom = uint24(pseudorandomness);
         for(uint16 i = 0; i < cTypeProbability.length; i ++) {
             if(partRandom < cTypeProbability[i]) {
@@ -28,6 +34,7 @@ contract NSeeder is ISeeder, Ownable {
             }
         }
         
+        // Pick up random skin tone
         partRandom = uint24(pseudorandomness >> 24);
         for(uint16 i = 0; i < cTypeProbability.length; i ++) {
             if(partRandom < cTypeProbability[i]) {
@@ -36,13 +43,32 @@ contract NSeeder is ISeeder, Ownable {
             }
         }
 
+        // Get possible groups for the current punk type
+        uint16 punkFlags = uint16(accFlags[seed.punkType]);
+        uint256[] memory usedGroupFlags = new uint256[](accGroup.length);
+        uint256[] memory availableGroups = new uint256[](accGroup.length);
+        uint256 availableGroupCount = 0;
+        for(uint8 acc = 0; punkFlags > 0; acc ++) {
+            if(punkFlags & 0x01 == 1) {
+                uint256 group = accGroupMapping[acc];
+                if(usedGroupFlags[group] == 1)
+                    availableGroups[availableGroupCount ++] = group;
+                else
+                    usedGroupFlags[group] = 1;
+            }
+            punkFlags >>= 1;
+        }
+
+        // Pick up random accessory count
         partRandom = uint24(pseudorandomness >> 48);
+        uint16 curAccCount = 0;
         for(uint16 i = 0; i < cAccProbability.length; i ++) {
-            if(partRandom < cAccProbability[i]) {
-                // set seed values here
+            if(uint256(partRandom) * 0xffffff / cAccProbability[availableGroupCount] < cAccProbability[i]) {
+                curAccCount = i;
                 break;
             }
         }
+        
         return seed;
     }
 
@@ -60,7 +86,18 @@ contract NSeeder is ISeeder, Ownable {
         delete cAccProbability;
         _setProbability(cAccProbability, probabilities);
     }
-    
+
+    function setAccAvailability(uint256 count, uint256[] calldata flags) external onlyOwner {
+        // i = 0;1;2;3;4
+        for(uint i = 0; i < flags.length; i ++)
+            accFlags[i] = flags[i];
+        accCount = count;
+    }
+    function setExclusiveAcc(uint256[] calldata exclusives) external onlyOwner {
+        
+    }
+
+
     function _setProbability(
         uint24[] storage cumulativeArray,
         uint256[] calldata probabilities
